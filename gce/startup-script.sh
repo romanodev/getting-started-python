b0683be6d750c1f737c5ad016fa25b42beae3aec
Giuseppe Romano <romanodev@pop-os.localdomain>
2021-03-10 11:06:19 -0500

    test

--- gce/startup-script.sh
+++ gce/startup-script.sh
@@ -30,7 +30,7 @@ useradd -m -d /home/pythonapp pythonapp
 
 # Fetch source code
 export HOME=/root
-git clone https://github.com/GoogleCloudPlatform/getting-started-python.git /opt/app
+git clone https://github.com/romanodev/getting-started-python.git /opt/app
 
 # Python environment setup
 virtualenv -p python3 /opt/app/gce/env
